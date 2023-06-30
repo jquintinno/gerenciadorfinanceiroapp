98249350075a1392705f469eddeda3b7b008b038
José Quintinno <jquintinno@gmail.com>
2023-06-30 10:15:53 -0300

    feature: Implementar cadastro de Lançamento Financeiro (Receita)

--- src/app/component/modal-receita/modal-receita.page.ts
+++ src/app/component/modal-receita/modal-receita.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonInput } from '@ionic/angular';
 import { RedirecionamentoTelaService } from 'src/app/service/redirecionamento-tela.service';
 
 @Component({
@@ -8,11 +9,20 @@ import { RedirecionamentoTelaService } from 'src/app/service/redirecionamento-te
 })
 export class ModalReceitaPage implements OnInit {
 
+  @ViewChild("autofocus", { static: false })
+  public ionInput: IonInput;
+
   constructor(
     private redirecionamentoTelaService: RedirecionamentoTelaService
   ) { }
 
   ngOnInit() { }
+
+  ionViewWillEnter() {
+    setTimeout(() => {
+      this.ionInput.setFocus();
+    }, 1300);
+  }
 
   public cadastrarReceita() {
     this.redirecionamentoTelaService.fecharModalController();
